d23f2c2fba15b32d3b4f1723a951cca741a19511
solis <torvalds@linux-foundation.org>
2022-03-18 23:52:21 -0300

    refactor long lines

--- main.py
+++ main.py
@@ -1,21 +1,26 @@
 from telegram import Update, InlineQueryResultPhoto, InputTextMessageContent
-from telegram.ext import Updater, CallbackContext, CommandHandler, InlineQueryHandler
+from telegram.ext import (Updater, CallbackContext, CommandHandler, 
+                          InlineQueryHandler)
 from uuid import uuid4
 from random import choices
 import sys
 import logging
 
-# Following tutorial in https://github.com/python-telegram-bot/python-telegram-bot/wiki/Extensions-%E2%80%93-Your-first-Bot
+# Following tutorial in 
+# https://github.com/python-telegram-bot/python-telegram-bot
+# /wiki/Extensions-%E2%80%93-Your-first-Bot
 
 def start(update: Update, context: CallbackContext):
     # Run when user enters /start
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Let's play Dixit!")
+    context.bot.send_message(chat_id=update.effective_chat.id,
+                             text="Let's play Dixit!")
 
 def inline(update: Update, context: CallbackContext):
-    # Supostamente roda quando o usuário faz um query inline, mas está se comportando bem mal
+    # Supostamente roda quando o usuário faz um query inline, 
+    # mas está se comportando bem mal
 
     def imagem_dixit_github(n):
-        # Fetches the n-th image from the github image repo and returns an InlineQueryResultPhoto
+        # Fetches the n-th image from the github image repo
         url_imagem = 'https://raw.githubusercontent.com/jminuscula/'\
                 + f'dixit-online/master/cards/card_{n:0>5}.jpg'
         print(url_imagem)
@@ -47,8 +52,8 @@ def run_bot(token):
     updater.idle()
 
 if __name__ == '__main__':
-    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                     level=logging.INFO)
+    logging_format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    logging.basicConfig(format=logging_format, level=logging.INFO)
     with open('token.txt', 'r') as token_file:
         token = token_file.readline()[:-1] # Remove \n at the end
         run_bot(token)
