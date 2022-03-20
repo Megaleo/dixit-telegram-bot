5801a5479b9cff31bd0155ac29e0710a4b58c571
Leonardo Almeida Lessa <leonardo.a.lessa@gmail.com>
2022-03-20 16:07:49 -0400

    Fixed inline by prohibiting players of playing more than one game at once.

--- main.py
+++ main.py
@@ -11,6 +11,31 @@ import game
 # Following tutorial in
 # https://github.com/python-telegram-bot/python-telegram-bot-wiki/Extensions-%E2%80%93-Your-first-Bot
 
+def get_active_games(context):
+    '''Returns all `DixitGame`'s that are in `context.dispatcher.chat_data`
+    as a dict of chat_id : dixit_game'''
+    # There must be a better way of doing this...
+    # In Haskell, I would have used `mapMaybe dixit_game chat_data` or smth
+    if context.dispatcher.chat_data:
+        dixit_game_dict = {chat_id : data['dixit_game']
+                           for chat_id, data in context.dispatcher.chat_data.items()
+                           if data.get('dixit_game', None)}
+        return dixit_game_dict
+    else:
+        return {}
+
+def find_user_games(context, user):
+    '''Finds the `chat_id`'s of the games where the `user` is playing.
+    Returns a dict of chat_id : dixit_game
+
+    This is useful to resolve the issue of the inline queries not having
+    the information about the chat from which it is requested.
+    With the information of the `chat_id`'s, we could either prohibit the
+    player from playing multiple games at once, or give him the choice of which
+    game they want to play at that time like the unobot does.'''
+    return {chat_id : dixit_game
+            for chat_id, dixit_game in get_active_games(context).items()
+            if user in dixit_game.get_user_list()}
 
 def ensure_game(callback):
     """Decorator to ensure a game exists before callbacks are made"""
@@ -26,18 +51,32 @@ def ensure_game(callback):
 
     return safe_callback
 
+def ensure_user_inactive(callback):
+    """Decorator to ensure the user is not in another game"""
+    def safe_callback(update, context):
+        # Checks if there is an ongoing game
+        user = update.message.from_user
+        if find_user_games(context, user):
+            context.bot.send_message(chat_id=update.effective_chat.id,
+                                     text=f"Damn you, {user.first_name}! "
+                                     "You are in another game already!")
+        else:
+            return callback(update, context)
 
+    return safe_callback
+
+@ensure_user_inactive
 def start_game_callback(update, context):
     '''Command callback. When /startgame is called, it does the following:
     1. Checks if there is an ongoing game
     1.1. If not, creates a default DixitGame instance and stores it in
-        context.chat_data (See more about this in 
+        context.chat_data (See more about this in
         https://github.com/python-telegram-bot/python-telegram-bot/wiki/Storing-bot,-user-and-chat-related-data#chat-migration,
-        and about making it persistent in the future in 
+        and about making it persistent in the future in
         https://github.com/python-telegram-bot/python-telegram-bot/wiki/Making-your-bot-persistent)
     2. Loads cards into game with random game_id's
     3. Sets this user as master
-    4. Joins master user to the game
+    4. Joins master user to the game IF they are not in other games
     '''
     user = update.message.from_user
     # Checks if there is no ongoing game
@@ -48,7 +87,7 @@ def start_game_callback(update, context):
     else:
         game_id_list = list(range(1, 101))
         shuffle(game_id_list) # Permutates the game_id of the cards at random
-        cards = [game.image_dixit_github(n, game_id_list[n-1]) 
+        cards = [game.image_dixit_github(n, game_id_list[n-1])
                  for n in range(1, 101)]
         dixit_game = game.DixitGame(cards=cards)
         context.chat_data['dixit_game'] = dixit_game
@@ -63,6 +102,7 @@ def start_game_callback(update, context):
                                       "has started a game.")
 
 @ensure_game
+@ensure_user_inactive
 def join_game_callback(update, context):
     '''Command callback. When /joingame is called, it does the following:
     1. Checks if user has already joined the game
@@ -80,7 +120,7 @@ def join_game_callback(update, context):
                                      text="There are only enough cards in the"
                                      f"game to supply {n_supported_players} "
                                      "players, unfortunately!")
-            # ask if they'd like to play with fewer cards per player?   
+            # ask if they'd like to play with fewer cards per player?
     else:
         player = game.Player(user)
         dixit_game.add_player(player)
@@ -127,62 +167,41 @@ def inline_callback(update, context):
     If stage == 3, then show chosen cards
     Otherwise, do nothing'''
     user = update.inline_query.from_user
-    if 'dixit_game' in context.chat_data.keys():
-        dixit_game = context.chat_data['dixit_game']
-        if user in dixit_game.get_user_list():
-            player_index = dixit_game.get_user_list().index(user)
-            player = dixit_game.players[player_index]
-            if dixit_game.stage in [1, 2]:
-                results = [InlineQueryResultPhoto(
-                           id = str(uuid4()),
-                           photo_url = card.photo_id,
-                           thumb_url = card.photo_id,
-                           title = f"Card {card.game_id} in the player's hand",
-                           input_message_content = InputTextMessageContent(
-                           f"{user.id}:{card.game_id}"
-                           ))
-                           for card in player.hand_cards]
+    dixit_game_dict = find_user_games(context, user)
+    if len(dixit_game_dict) == 1:
+        dixit_game = list(dixit_game_dict.values())[0]
+        player_index = dixit_game.get_user_list().index(user)
+        player = dixit_game.players[player_index]
+        if dixit_game.stage in [1, 2]:
+            results = [InlineQueryResultPhoto(
+                       id = str(uuid4()),
+                       photo_url = card.photo_id,
+                       thumb_url = card.photo_id,
+                       title = f"Card {card.game_id} in the player's hand",
+                       input_message_content = InputTextMessageContent(
+                       f"{user.id}:{card.game_id}"
+                       ))
+                       for card in player.hand_cards]
 
-            elif dixit_game.stage == 3:
-                results = [InlineQueryResultPhoto(
-                           id = str(uuid4()),
-                           photo_url = card.photo_id,
-                           thumb_url = card.photo_id,
-                           title = f"Chosen card {card.game_id}",
-                           input_message_content = InputTextMessageContent(
-                           f"{user.id}:{card.game_id}"
-                           ))
-                           for card in dixit_game.chosen_cards]
-            else:
-                results = [] # gostaria de botar um texto, mas n vi como
+        elif dixit_game.stage == 3:
+            results = [InlineQueryResultPhoto(
+                       id = str(uuid4()),
+                       photo_url = card.photo_id,
+                       thumb_url = card.photo_id,
+                       title = f"Chosen card {card.game_id}",
+                       input_message_content = InputTextMessageContent(
+                       f"{user.id}:{card.game_id}"
+                       ))
+                       for card in dixit_game.chosen_cards]
+        else:
+            results = [] # gostaria de botar um texto, mas n vi como
 
-            update.inline_query.answer(results) 
-
-
-# deprecado?        
-def inline(update: Update, context: CallbackContext):
-    # Roda quando o usuário faz um query inline,
-
-    def imagem_dixit_github(n):
-        # Fetches the n-th image from the github image repo
-        url_imagem = 'https://raw.githubusercontent.com/jminuscula/'\
-                + f'dixit-online/master/cards/card_{n:0>5}.jpg'
-        print(url_imagem)
-        return InlineQueryResultPhoto(
-                id = str(uuid4()),
-                photo_url = url_imagem,
-                thumb_url = url_imagem,
-                title = f"card {n} in the player's hand")
-
-    results = [imagem_dixit_github(n) for n in choices(range(1, 101), k=6)]
-    print()
-    update.inline_query.answer(results)
-
+        update.inline_query.answer(results)
 
 def run_bot(token):
     updater = Updater(token, use_context=True)
     dispatcher = updater.dispatcher
-    
+
     # Add commands handlers
     command_callbacks = {'startgame': start_game_callback,
                          'joingame': join_game_callback,
