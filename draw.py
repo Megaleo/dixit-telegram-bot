b0906d52bdc06f4cb54ea6fa8e030ace009e19c2
Leonardo Almeida Lessa <leonardo.a.lessa@gmail.com>
2022-04-03 23:39:26 -0400

    Implemented results' picture in actual game.

---
+++ draw.py
@@ -0,0 +1,205 @@
+from game import *
+from utils import *
+from telegram import User
+from urllib.request import urlopen
+from PIL import Image
+from cairo import Context, SVGSurface, ImageSurface, Surface, Error, FONT_SLANT_NORMAL, FONT_WEIGHT_NORMAL, FORMAT_ARGB32
+import math
+
+def draw_card(ctx, card):
+    file_jpeg = urlopen(card.url)
+    pil_file_jpeg = Image.open(file_jpeg)
+    png_filename = f'tmp/card_{card.image_id:0>5}.png'
+    pil_file_jpeg.save(png_filename)
+    card_surface = ImageSurface.create_from_png(png_filename)
+
+    ctx.scale(1/card_surface.get_width(), 1/card_surface.get_height())
+    ctx.set_source_surface(card_surface, 0, 0)
+    ctx.paint()
+    ctx.scale(card_surface.get_width(), card_surface.get_height())
+
+def draw_profile_pic(ctx,
+                     pic_filename,
+                     border_color=-1,
+                     default_filename='assets/default_pic.png'):
+    '''Draws profile picture in pic_filename. Just clips a circle around the image.
+    If image cannot be found, uses the default_filename'''
+    # Assuming pic is a png
+    # Ref: https://python-telegram-bot.readthedocs.io/en/stable/telegram.bot.html?highlight=getuserprofile#telegram.Bot.get_user_profile_photos
+    # bot.get_user_profile_photos(user_id, ...) --returns--> UserProfilePhotos --.photos[0][0]--> PhotoSize --.get_file()--> File --download(custom_path)--> Baixou o arquivo finalmente
+    try:
+        pic_surface = ImageSurface.create_from_png(pic_filename)
+    except Error:
+        pic_surface = ImageSurface.create_from_png(default_filename)
+
+    ctx.arc(0.5, 0.5, 0.5, 0, 2*math.pi)
+    ctx.clip()
+
+    ctx.scale(1/pic_surface.get_width(), 1/pic_surface.get_height())
+    ctx.set_source_surface(pic_surface, 0, 0)
+    ctx.paint()
+    ctx.scale(pic_surface.get_width(), pic_surface.get_height())
+
+    ctx.reset_clip()
+
+    if border_color != -1:
+        ctx.arc(0.5, 0.5, 0.5, 0, 2*math.pi)
+        ctx.set_source_rgba(*border_color)
+        ctx.set_line_width(0.05)
+        ctx.stroke()
+
+def draw_background(ctx,
+                    clip_width,
+                    clip_height,
+                    background_filename='assets/paper_background.png'):
+    '''Draws a repeating background on a given clipped area'''
+    background_surface = ImageSurface.create_from_png(background_filename)
+
+    ctx.scale(1/clip_width, 1/clip_height)
+    ctx.rectangle(0, 0, clip_width, clip_height)
+    ctx.clip()
+
+    for x in range(1 + clip_width // background_surface.get_width()):
+        for y in range(1 + clip_height // background_surface.get_height()):
+            ctx.set_source_surface(background_surface,
+                                   x*background_surface.get_width(),
+                                   y*background_surface.get_height())
+            ctx.paint()
+
+    ctx.scale(clip_width, clip_height)
+
+    ctx.reset_clip()
+
+card_hor_border=0.2
+score_height=0.15
+voted_pic_diam=0.3
+voter_pic_diam=0.2
+card_aspect_ratio=1.5
+results_border=0.1
+clue_height=0.2
+
+player_border_color = (0.1, 0.1, 0.1)
+storyteller_border_color = (0.9, 0.9, 0)
+score_color = (0.0, 0.0, 0.0)
+clue_color = (0.0, 0.0, 0.0)
+delta_score_color = (0.0, 0.6, 0.0)
+
+card_width=236/2
+def draw_results(ctx, results):
+    total_width = (1 + 2*card_hor_border)*len(results.players) + 2*results_border
+    total_height = voted_pic_diam + score_height + card_aspect_ratio + voter_pic_diam + clue_height + 2*results_border
+
+    # Draw background
+    draw_background(ctx, int(card_width*total_width), int(card_width*total_height))
+
+    ctx.scale(1/total_width,
+              1/total_height)
+    ctx.select_font_face("Arial",
+                         FONT_SLANT_NORMAL,
+                         FONT_WEIGHT_NORMAL)
+    # Write clue
+    ctx.translate(0, total_height - results_border)
+
+    ctx.set_source_rgb(*clue_color)
+    ctx.set_font_size(clue_height)
+    clue_extents = ctx.text_extents(results.clue)
+    ctx.translate(total_width/2 - clue_extents.width / 2 - clue_extents.x_bearing,
+                  (clue_extents.height-clue_height)/2)
+
+    ctx.show_text(results.clue)
+
+    ctx.translate(-(total_width/2 - clue_extents.width / 2 - clue_extents.x_bearing),
+                  -(clue_extents.height-clue_height)/2)
+    ctx.translate(0, -(total_height - results_border))
+
+    ctx.translate(results_border, results_border)
+    for player in results.players:
+        ctx.translate(card_hor_border, 0)
+
+        # Draw star in storyteller
+        if player == results.storyteller:
+            border_color = storyteller_border_color
+        else:
+            border_color = player_border_color
+
+        # Draw profile pic above card
+        ctx.translate(1/2 - voted_pic_diam/2, 0)
+        ctx.scale(voted_pic_diam, voted_pic_diam)
+
+        draw_profile_pic(ctx, f'tmp/pic_{player.id}.png', border_color=border_color) # modify this later
+
+        ctx.scale(1/voted_pic_diam, 1/voted_pic_diam)
+        ctx.translate(-(1/2 - voted_pic_diam/2), 0)
+
+        # Draw total and delta scores
+        ctx.translate(0, voted_pic_diam + score_height)
+
+        ctx.set_source_rgb(*score_color)
+        score_text = str(results.score[player])
+        ctx.set_font_size(score_height)
+        score_extents = ctx.text_extents(score_text)
+        ctx.translate(1/2 - score_extents.width / 2 - score_extents.x_bearing,
+                      (score_extents.height-score_height)/2)
+
+        ctx.show_text(score_text)
+
+        ctx.set_source_rgb(*delta_score_color)
+        delta_score_text = f'+{results.delta_score[player]}'
+        ctx.set_font_size(score_height/2)
+        ctx.show_text(delta_score_text)
+
+        ctx.translate(-(1/2 - score_extents.width / 2 - score_extents.x_bearing),
+                      -(score_extents.height-score_height)/2)
+
+
+        ctx.translate(0, -(voted_pic_diam + score_height))
+
+        # Draw card
+
+        ctx.translate(0, score_height + voted_pic_diam)
+        ctx.scale(1, card_aspect_ratio)
+
+        draw_card(ctx, results.table[player])
+
+        ctx.scale(1, 1/card_aspect_ratio)
+        ctx.translate(0, - (score_height + voted_pic_diam))
+
+        # Draw profile pics below card
+        # TODO what to do when there are many players
+        n_voters=0
+        for voter, voted in results.votes.items():
+            if voted == player:
+                ctx.translate(n_voters*(voted_pic_diam),
+                              voted_pic_diam + score_height + card_aspect_ratio)
+
+                ctx.scale(voter_pic_diam, voter_pic_diam)
+                draw_profile_pic(ctx, f'tmp/pic_{voter.id}.png')
+                ctx.scale(1/voter_pic_diam, 1/voter_pic_diam)
+
+                ctx.translate(-n_voters*(voter_pic_diam),
+                              -(voted_pic_diam + score_height + card_aspect_ratio))
+                n_voters += 1
+
+        # Translate to next card
+        ctx.translate(1 + card_hor_border, 0)
+    ctx.translate(-results_border, -results_border)
+
+def save_results_pic(results, n=0, card_width=236):
+    '''Saves results picture in tmp/results_pic_{n}.png. Returns filename'''
+    filename = f'tmp/results_pic_{n}.png'
+    total_width = (1 + 2*card_hor_border)*len(results.players) + 2*results_border
+    total_height = voted_pic_diam + score_height + card_aspect_ratio + voter_pic_diam + clue_height + 2*results_border
+
+    width = int(card_width*total_width)
+    height = int(card_width*total_height)
+
+    surface = ImageSurface(FORMAT_ARGB32, width, height)
+    ctx = Context(surface)
+
+    ctx.scale(width, height)
+
+    draw_results(ctx, results)
+
+    surface.write_to_png(filename)
+
+    return filename
